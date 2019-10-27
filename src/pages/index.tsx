417baf0d1c7c9d9a091cc9492c7b12e9115fd1d1
Matej Chalk <matejchalk@gmail.com>
2019-10-27 12:43:58 +0100

    style(index): prefer functional component over class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Link } from 'gatsby';
 import { css } from 'emotion';
+import { Link } from 'gatsby';
+import React from 'react';
 
 import Layout from '../components/layout';
 
@@ -8,16 +8,12 @@ const header = css({
   color: '#0505',
 });
 
-class IndexPage extends Component {
-  render() {
-    return (
-      <Layout>
-        <h1 className={header}>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Layout>
-    );
-  }
-}
+const IndexPage = () => (
+  <Layout>
+    <h1 className={header}>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+);
 export default IndexPage;
